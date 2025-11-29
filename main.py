2cc47d92a2cd895e31dc416b176eb76d480634d9
Salwa Azariouh <salwaazariouh@gmail.com>
2025-11-29 17:08:18 +0100

    Adding a switch scores button Fixes #1

--- main.py
+++ main.py
@@ -46,3 +46,10 @@ async def change_name(team: int = Path(..., ge=0, le=1), name: str = Body()):
     global state
     state[team].name = name
     return state[team]
+
+
+@app.put("/switch", response_model=list[Team])
+async def switch():
+    global state
+    state[1], state[0] = state[0], state[1]
+    return state
